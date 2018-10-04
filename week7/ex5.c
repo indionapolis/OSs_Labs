f7a0fb3be71c645bd99e905d2eb9e071d7554c60
indionapolis <panddromas@gmail.com>
2018-10-04 19:41:42 +0300

    fix

--- week7/ex5.c
+++ week7/ex5.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main() {
-    char **s;
+    char *s;
     char foo[] = "Hello World";
     s = foo;
     printf("s is %s\n", s);
-    s[0] = foo;
-    printf("s[0] is %s\n", s[0]);
+    s[0] = foo[0];
+    printf("s[0] is %c\n", s[0]);
     return(0);
 }
